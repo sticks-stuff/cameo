a4981386d406755d642dbb96a8e20f38842e3e24
charlotte <charlottemackenzi@gmail.com>
2023-03-24 17:11:30 +1300

    added a bunch of logs

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ if (!fs.existsSync(serveDir)){
 }
 
 app.get('/', (req, res) => {
+
+	console.log("here1")
 	if(req.query.name1 === undefined || req.query.type === undefined) {
 		res.send("test successful");
 		return;
@@ -25,8 +27,9 @@ app.get('/', (req, res) => {
 	var clips = new Array();
 	var outputNameAsString = [req.query.type, req.query.name1, req.query.name2].filter(Boolean).join("_") + ".webm"; //we use .filter and .join instead of concatenate because name2 might not always exist
 	
+	console.log("here2")
 	console.log(outputNameAsString);
-
+	
 	const varServedFiles = fs.readdirSync(serveDir);
 
 	if(varServedFiles.includes(outputNameAsString)) {
@@ -36,45 +39,49 @@ app.get('/', (req, res) => {
 		res.sendFile(outputNameAsString, {
 			root: __dirname + "/serve"
 		});
+		console.log("here3")
 	} else {
-
+		
+		console.log("here4")
 		switch (req.query.type) {
 			case "anniversary":
-				if (heyNames.includes(req.query.name1 + '.webm')) { //we always want anniversary videos to start with "hey" if we can
-					clips.push({"fileName": "videos/names/hey/" + req.query.name1 + ".webm"}); //rather than stripping every single file name in the array of the phrase ".webm", lets just compare with .webm added
-					clips.push({"fileName": "videos/and.webm"});
-					clips.push({"fileName": "videos/names/" + req.query.name2 + ".webm"});
-				} else if (heyNames.includes(req.query.name2 + '.webm')) {
-					clips.push({"fileName": "videos/names/hey/" + req.query.name2 + ".webm"});
-					clips.push({"fileName": "videos/and.webm"});
-					clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
-				} else {
-					clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
-					clips.push({"fileName": "videos/and.webm"});
-					clips.push({"fileName": "videos/names/" + req.query.name2 + ".webm"});
-				}
-				clips.push({"fileName": "videos/anniversary.webm"});
-				break;
-
+			if (heyNames.includes(req.query.name1 + '.webm')) { //we always want anniversary videos to start with "hey" if we can
+				clips.push({"fileName": "videos/names/hey/" + req.query.name1 + ".webm"}); //rather than stripping every single file name in the array of the phrase ".webm", lets just compare with .webm added
+				clips.push({"fileName": "videos/and.webm"});
+				clips.push({"fileName": "videos/names/" + req.query.name2 + ".webm"});
+			} else if (heyNames.includes(req.query.name2 + '.webm')) {
+				clips.push({"fileName": "videos/names/hey/" + req.query.name2 + ".webm"});
+				clips.push({"fileName": "videos/and.webm"});
+				clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+			} else {
+				clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+				clips.push({"fileName": "videos/and.webm"});
+				clips.push({"fileName": "videos/names/" + req.query.name2 + ".webm"});
+			}
+			clips.push({"fileName": "videos/anniversary.webm"});
+			break;
+			
 			case "birthday_type01":
-				clips.push({"fileName": "videos/birthday_type01_01.webm"});
-				clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
-				clips.push({"fileName": "videos/birthday_type01_02.webm"});
-				clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
-				clips.push({"fileName": "videos/birthday_type01_03.webm"});
-				break;
+			console.log("here5")
+			clips.push({"fileName": "videos/birthday_type01_01.webm"});
+			clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+			clips.push({"fileName": "videos/birthday_type01_02.webm"});
+			clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+			clips.push({"fileName": "videos/birthday_type01_03.webm"});
+			break;
 			
 			case "birthday_type02":
-				clips.push({"fileName": "videos/birthday_type02_01.webm"});
-				clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
-				clips.push({"fileName": "videos/birthday_type02_02.webm"});
-				clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
-				clips.push({"fileName": "videos/birthday_type02_03.webm"});
-				break;
+			clips.push({"fileName": "videos/birthday_type02_01.webm"});
+			clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+			clips.push({"fileName": "videos/birthday_type02_02.webm"});
+			clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+			clips.push({"fileName": "videos/birthday_type02_03.webm"});
+			break;
 		}
-
+		
 		// console.log(clips);
 		console.log("producing new video " + outputNameAsString);
+		console.log("here6")
 		
 		// FFMpeg doesn't like relative urls, especially in 3rd-party modules, so we'll convert them to absolute instead
 		clips = clips.map(i => {
@@ -82,6 +89,7 @@ app.get('/', (req, res) => {
 			return i;
 		});
 		
+		console.log("here7")
 		videoConcat({
 			silent: false, // if set to false, gives detailed output on console
 			overwrite: true //lets keep this at true just on the off chance that checking if the file exists fails
@@ -90,6 +98,7 @@ app.get('/', (req, res) => {
 		.output("serve/" + outputNameAsString)
 		.concat()
 		.then((outputFileName) => {
+			console.log("here8")
 			console.log("produced new video " + outputNameAsString);
 			res.set("Content-Disposition", "attachment;filename=" + outputFileName);
 			res.attachment(outputFileName);
